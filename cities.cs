110f11fb968608925ddfd2dfc3cf7fea32df00e1
HarriHiie <89829583+HarriHiie@users.noreply.github.com>
2023-02-14 10:50:19 +0200

    Add files via upload

---
+++ cities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cities = new Dictionary<string, string>();
+            {
+                {"Harju", "Tallinn"},
+                {"Läänemaa", "Haapsalu"},
+                {"Raplamaa", "Rapla"},
+            };
+
+            cities.Remove("Harju");
+
+            if (cities.ContainsKey("Läänemaa"))
+            {
+                cities.Remove("Läänemaa");
+            }
+
+            Console.WriteLine("total elements: {0}", cities.Count);
+        }
+    }
+}
